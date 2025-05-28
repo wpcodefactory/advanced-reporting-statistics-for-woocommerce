51b22d314b2782cb58ee766d97dec6179f8c3e26
algoritmika <info@algoritmika.com>
2025-05-28 18:32:43 +0300

    Cleanup

--- webd-woocommerce-reporting-statistics.php
+++ webd-woocommerce-reporting-statistics.php
@@ -52,186 +52,190 @@ include_once( plugin_dir_path(__FILE__) ."/class-admin.php");
  */
 class webdWoocommerceReportingStatistics extends webdWoocommerceReportingStatisticsAdmin {
 
-		public $plugin       = 'webdWoocommerceReportingStatistics';
-		public $name         = 'Webd Woocommerce Advanced Reporting & Statistics';
-		public $slug         = 'webd-woocommerce-reporting-statistics';
-		public $proUrl       = 'https://extend-wp.com/product/woocommerce-advanced-reporting-statistics/';
+	public $plugin = 'webdWoocommerceReportingStatistics';
+	public $name   = 'Webd Woocommerce Advanced Reporting & Statistics';
+	public $slug   = 'webd-woocommerce-reporting-statistics';
+	public $proUrl = 'https://extend-wp.com/product/woocommerce-advanced-reporting-statistics/';
 
-		/**
-		 * Constructor.
-		 *
-		 * @version 4.0.0
-		 */
-		public function __construct() {
+	/**
+	 * Constructor.
+	 *
+	 * @version 4.0.0
+	 */
+	public function __construct() {
 
-			add_action('admin_enqueue_scripts', array($this, 'BackEndScripts') );
+		add_action('admin_enqueue_scripts', array($this, 'BackEndScripts') );
 
-			add_action( 'wpfactory_wc_ars_output_settings', array( $this, 'init' ) );
+		add_action( 'wpfactory_wc_ars_output_settings', array( $this, 'init' ) );
 
-			register_activation_hook( __FILE__,  array($this, 'onActivation') );
-			register_deactivation_hook( __FILE__,  array($this, 'onDeactivation') );
+		register_activation_hook( __FILE__,  array($this, 'onActivation') );
 
-			add_action("admin_init", array($this, 'settingsSection') );
+		add_action("admin_init", array($this, 'settingsSection') );
 
-			if( isset( $_GET['page'] ) && $_GET['page'] == 'webd-woocommerce-reporting-statistics' ) {
-				add_action("admin_footer", array($this,"proModal" ) );
-			}
-
-
-
-			add_action( 'wp_ajax_nopriv_stat_extensions', array( $this,'extensions' ) );
-			add_action( 'wp_ajax_stat_extensions', array( $this,'extensions' ) );
-
-			// deactivation survey
-
-			include( plugin_dir_path(__FILE__) .'/lib/codecabin/plugin-deactivation-survey/deactivate-feedback-form.php');
-			add_filter('codecabin_deactivate_feedback_form_plugins', function($plugins) {
-
-				$plugins[] = (object)array(
-					'slug'		=> 'webd-woocommerce-advanced-reporting-statistics',
-					'version'	=> '3.1'
-				);
-
-				return $plugins;
-
-			});
-
-			register_activation_hook( __FILE__, array( $this, 'notification_hook' ) );
-
-			add_action( 'admin_notices', array( $this,'notification' ) );
-			add_action( 'wp_ajax_nopriv_push_not',array( $this, 'push_not'  ) );
-			add_action( 'wp_ajax_push_not', array( $this, 'push_not' ) );
-
-
+		if( isset( $_GET['page'] ) && $_GET['page'] == 'webd-woocommerce-reporting-statistics' ) {
+			add_action("admin_footer", array($this,"proModal" ) );
 		}
 
-		/**
-		 * notification.
-		 *
-		 * @version 4.0.0
-		 */
-		public function notification(){
+		add_action( 'wp_ajax_nopriv_stat_extensions', array( $this,'extensions' ) );
+		add_action( 'wp_ajax_stat_extensions', array( $this,'extensions' ) );
 
-			$screen = get_current_screen();
-			if ( 'wpfactory_page_webd-woocommerce-reporting-statistics'  !== $screen->base )
-			return;
+		// Deactivation survey
+		include( plugin_dir_path(__FILE__) .'/lib/codecabin/plugin-deactivation-survey/deactivate-feedback-form.php');
+		add_filter( 'codecabin_deactivate_feedback_form_plugins', function( $plugins ) {
 
-			/* Check transient, if available display notice */
-			if( get_transient( $this->plugin ."_notification" ) ){
-				?>
-				<div class="updated notice  webdWoocommerceReportingStatistics_notification">
-					<a href="#" class='dismiss' style='float:right;padding:4px' >close</a>
-					<h4><i><?php esc_attr( print $this->name );?> | <?php esc_html_e( "Add your Email below & get ", 'imue' ); ?><strong><?php esc_html_e( "discounts", 'webd-woocommerce-reporting-statistics' ); ?></strong><?php esc_html_e( " in our pro plugins at", 'webd-woocommerce-reporting-statistics' ); ?> <a href='https://extend-wp.com' target='_blank' >extend-wp.com!</a></i></h4>
-
-					<form method='post' id='webdWoocommerceReportingStatistics_signup'>
-						<p>
-						<input required type='email' name='woopei_email' />
-						<input required type='hidden' name='product' value='2071' />
-						<input type='submit' class='button button-primary' name='submit' value='<?php esc_html_e("Sign up", "webd-woocommerce-reporting-statistics" ); ?>' />
-						</p>
-					</form>
-				</div>
-				<?php
-			}
-		}
-
-
-		public function push_not(){
-
-			delete_transient( $this->plugin ."_notification" );
-
-		}
-		public function notification_hook() {
-			set_transient( $this->plugin ."_notification", true );
-		}
-
-		public function onActivation(){
-			require_once(ABSPATH .'/wp-admin/includes/plugin.php');
-			$pro = "/woocommerce-reporting-statistics-pro/woocommerce-reporting-statistics-pro.php";
-			deactivate_plugins($pro);
-
-			$order_fields = array(  "_id","_date_created", "_status",  "_payment_method_title","_coupon_codes" , "_billing_first_name" , "_billing_last_name" ,  "_billing_country", "_shipping_total" , "_total_discount" , "_total_tax" , "_total_refunded" ,"_subtotal" , "_total" );
-			update_option($this->plugin."_custom_fields", $order_fields );
-
-			$default_status = ['wc-completed', 'wc-processing', 'wc-on-hold', 'wc-refunded' ];
-			update_option( $this->plugin.'_status' , $default_status );
-
-		}
-
-		public function onDeactivation() {
-
-		}
-
-		/**
-		 * BackEndScripts.
-		 *
-		 * @version 4.0.0
-		 */
-		public function BackEndScripts(){
-
-			$screen = get_current_screen();
-			if ( 'wpfactory_page_webd-woocommerce-reporting-statistics'  !== $screen->base )
-			return;
-
-			wp_enqueue_style( "webd-woocommerce-reporting-statistics"."adminCss", plugins_url( "/css/backend.css?v=bvc", __FILE__ ) );
-			wp_enqueue_style( "webd-woocommerce-reporting-statistics"."adminCss");
-
-			wp_enqueue_script('jquery');
-			wp_enqueue_script( 'jquery-ui-datepicker' );
-			wp_enqueue_style( 'jquery-ui-style', plugins_url( "/css/jquery-ui.css", __FILE__ ), true);
-			wp_enqueue_script('jquery-ui-accordion');
-			wp_enqueue_script("jquery-ui-tabs");
-			wp_enqueue_script( "webd-woocommerce-reporting-statistics"."charts", plugins_url( "/js/chart.js", __FILE__ ), null, true);
-			wp_enqueue_script( "webd-woocommerce-reporting-statistics"."adminJs", plugins_url( "/js/backend.js?v=bvc", __FILE__ ) , array('jquery','jquery-ui-accordion','jquery-ui-tabs','jquery-ui-datepicker') , null, true);
-
-			wp_enqueue_style( "webd-woocommerce-reporting-statistics"."_fa", plugins_url( '/css/font-awesome.min.css', __FILE__ ));
-
-			$page = $this->slug;
-			$tab = array( 'all' );
-			$custom_fields = get_option( $this->plugin."_custom_fields" );
-
-			$queryLimit = get_option( $this->plugin."queryLimit" , 500 );
-
-			wp_localize_script(
-				"webd-woocommerce-reporting-statistics"."adminJs",
-				"webdWoocommerceReportingStatistics",
-				array(
-					'thispluginpage' => admin_url("/admin.php?page=".$this->slug),
-					'url'            => admin_url( 'admin-ajax.php' ),
-					'plugin_url'     => plugins_url( '', __FILE__ ),
-					'ajax_url'       => admin_url( 'admin-ajax.php' ),
-					'siteUrl'        => site_url(),
-					'plugin_wrapper' => "webd-woocommerce-reporting-statistics",
-					'select'         => esc_html__( 'Select...', $this->plugin ),
-					'orders_loading' => esc_html__ ( "Your orders are loading please wait...", $this->plugin ),
-					'orders_loaded'  => esc_html__ ( "All orders are loaded!", $this->plugin ),
-					'no_orders'      => esc_html__ ( "No orders found...", $this->plugin ),
-					'page'           => $page,
-					'tab'            => $tab,
-					'custom_fields'  => $custom_fields,
-					'currency'       => get_woocommerce_currency_symbol(),
-					'limit'          => $queryLimit,
-				)
+			$plugins[] = (object) array(
+				'slug'    => 'webd-woocommerce-advanced-reporting-statistics',
+				'version' => '3.1',
 			);
-			wp_enqueue_script( "webd-woocommerce-reporting-statistics"."adminJs");
-		}
 
-		public function init(){
-			print "<div class='". $this->plugin ."'>";
-			$this->adminHeader();
-			$this->adminSettings();
-			$this->adminFooter();
-			print "</div>";
-		}
-		public function initStats(){
-			print "<div class='". $this->plugin ."'>";
-			$this->adminHeader();
-			echo "<h3>". esc_html__('Statistics',"webd-woocommerce-reporting-statistics") ."</h3>";
-			echo do_shortcode( '[adStats]' );
+			return $plugins;
 
-			$this->adminFooter();
-			print "</div>";
+		});
+
+		register_activation_hook( __FILE__, array( $this, 'notification_hook' ) );
+
+		add_action( 'admin_notices', array( $this,'notification' ) );
+		add_action( 'wp_ajax_nopriv_push_not',array( $this, 'push_not' ) );
+		add_action( 'wp_ajax_push_not', array( $this, 'push_not' ) );
+
+	}
+
+	/**
+	 * notification.
+	 *
+	 * @version 4.0.0
+	 */
+	public function notification() {
+
+		$screen = get_current_screen();
+		if ( 'wpfactory_page_webd-woocommerce-reporting-statistics'  !== $screen->base )
+		return;
+
+		/* Check transient, if available display notice */
+		if( get_transient( $this->plugin ."_notification" ) ){
+			?>
+			<div class="updated notice  webdWoocommerceReportingStatistics_notification">
+				<a href="#" class='dismiss' style='float:right;padding:4px' >close</a>
+				<h4><i><?php esc_attr( print $this->name );?> | <?php esc_html_e( "Add your Email below & get ", 'imue' ); ?><strong><?php esc_html_e( "discounts", 'webd-woocommerce-reporting-statistics' ); ?></strong><?php esc_html_e( " in our pro plugins at", 'webd-woocommerce-reporting-statistics' ); ?> <a href='https://extend-wp.com' target='_blank' >extend-wp.com!</a></i></h4>
+
+				<form method='post' id='webdWoocommerceReportingStatistics_signup'>
+					<p>
+					<input required type='email' name='woopei_email' />
+					<input required type='hidden' name='product' value='2071' />
+					<input type='submit' class='button button-primary' name='submit' value='<?php esc_html_e("Sign up", "webd-woocommerce-reporting-statistics" ); ?>' />
+					</p>
+				</form>
+			</div>
+			<?php
 		}
+	}
+
+	/**
+	 * push_not.
+	 */
+	public function push_not() {
+		delete_transient( $this->plugin ."_notification" );
+	}
+
+	/**
+	 * notification_hook.
+	 */
+	public function notification_hook() {
+		set_transient( $this->plugin ."_notification", true );
+	}
+	/**
+	 * onActivation.
+	 */
+	public function onActivation() {
+		require_once(ABSPATH .'/wp-admin/includes/plugin.php');
+		$pro = "/woocommerce-reporting-statistics-pro/woocommerce-reporting-statistics-pro.php";
+		deactivate_plugins($pro);
+
+		$order_fields = array(  "_id","_date_created", "_status",  "_payment_method_title","_coupon_codes" , "_billing_first_name" , "_billing_last_name" ,  "_billing_country", "_shipping_total" , "_total_discount" , "_total_tax" , "_total_refunded" ,"_subtotal" , "_total" );
+		update_option($this->plugin."_custom_fields", $order_fields );
+
+		$default_status = ['wc-completed', 'wc-processing', 'wc-on-hold', 'wc-refunded' ];
+		update_option( $this->plugin.'_status' , $default_status );
+
+	}
+
+	/**
+	 * BackEndScripts.
+	 *
+	 * @version 4.0.0
+	 */
+	public function BackEndScripts() {
+
+		$screen = get_current_screen();
+		if ( 'wpfactory_page_webd-woocommerce-reporting-statistics'  !== $screen->base )
+		return;
+
+		wp_enqueue_style( "webd-woocommerce-reporting-statistics"."adminCss", plugins_url( "/css/backend.css?v=bvc", __FILE__ ) );
+		wp_enqueue_style( "webd-woocommerce-reporting-statistics"."adminCss");
+
+		wp_enqueue_script('jquery');
+		wp_enqueue_script( 'jquery-ui-datepicker' );
+		wp_enqueue_style( 'jquery-ui-style', plugins_url( "/css/jquery-ui.css", __FILE__ ), true);
+		wp_enqueue_script('jquery-ui-accordion');
+		wp_enqueue_script("jquery-ui-tabs");
+		wp_enqueue_script( "webd-woocommerce-reporting-statistics"."charts", plugins_url( "/js/chart.js", __FILE__ ), null, true);
+		wp_enqueue_script( "webd-woocommerce-reporting-statistics"."adminJs", plugins_url( "/js/backend.js?v=bvc", __FILE__ ) , array('jquery','jquery-ui-accordion','jquery-ui-tabs','jquery-ui-datepicker') , null, true);
+
+		wp_enqueue_style( "webd-woocommerce-reporting-statistics"."_fa", plugins_url( '/css/font-awesome.min.css', __FILE__ ));
+
+		$page = $this->slug;
+		$tab = array( 'all' );
+		$custom_fields = get_option( $this->plugin."_custom_fields" );
+
+		$queryLimit = get_option( $this->plugin."queryLimit" , 500 );
+
+		wp_localize_script(
+			"webd-woocommerce-reporting-statistics"."adminJs",
+			"webdWoocommerceReportingStatistics",
+			array(
+				'thispluginpage' => admin_url("/admin.php?page=".$this->slug),
+				'url'            => admin_url( 'admin-ajax.php' ),
+				'plugin_url'     => plugins_url( '', __FILE__ ),
+				'ajax_url'       => admin_url( 'admin-ajax.php' ),
+				'siteUrl'        => site_url(),
+				'plugin_wrapper' => "webd-woocommerce-reporting-statistics",
+				'select'         => esc_html__( 'Select...', $this->plugin ),
+				'orders_loading' => esc_html__ ( "Your orders are loading please wait...", $this->plugin ),
+				'orders_loaded'  => esc_html__ ( "All orders are loaded!", $this->plugin ),
+				'no_orders'      => esc_html__ ( "No orders found...", $this->plugin ),
+				'page'           => $page,
+				'tab'            => $tab,
+				'custom_fields'  => $custom_fields,
+				'currency'       => get_woocommerce_currency_symbol(),
+				'limit'          => $queryLimit,
+			)
+		);
+		wp_enqueue_script( "webd-woocommerce-reporting-statistics"."adminJs");
+	}
+
+	/**
+	 * init.
+	 */
+	public function init() {
+		print "<div class='". $this->plugin ."'>";
+		$this->adminHeader();
+		$this->adminSettings();
+		$this->adminFooter();
+		print "</div>";
+	}
+
+	/**
+	 * initStats.
+	 */
+	public function initStats() {
+		print "<div class='". $this->plugin ."'>";
+		$this->adminHeader();
+		echo "<h3>". esc_html__('Statistics',"webd-woocommerce-reporting-statistics") ."</h3>";
+		echo do_shortcode( '[adStats]' );
+
+		$this->adminFooter();
+		print "</div>";
+	}
 
 }
 
